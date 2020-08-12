252af111981bc585eb8e4aca66d2d3b19ae6ab4a
Giorgio <giorgio.meretti@mail.polimi.it>
2020-08-12 22:47:51 +0200

    solved issue R

--- R/checkSmoothingParameters_time.R
+++ R/checkSmoothingParameters_time.R
@@ -87,10 +87,10 @@ checkSmoothingParameters_time<-function(locations = NULL, time_locations=NULL, o
       stop("'BC_indices' required in BC;  is NULL.")
     if (is.null(BC$BC_values))
       stop("'BC_indices' required in BC;  is NULL.")
-    if(DOF_evaluation == 'exact')
+    if(optimization == 'newton')
     {
-      DOF_evaluation = 'stochastic'
-      warning("'exact' 'DOF_evaluation' can't be performed with non-NULL boundary conditions, using 'stochastic' evaluation")
+      optimization = 'newton_fd'
+      warning("'newton' 'optimization' can't be performed with non-NULL boundary conditions, using 'newton_fd' instead")
     }
   }
   
